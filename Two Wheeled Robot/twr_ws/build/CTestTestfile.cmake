17554183249e7bd04da29419b5057be8a00e6d29
Dipam Patel <dipu18@terpmail.umd.edu>
2019-08-17 08:09:50 -0400

    Updated Files

---
+++ Two Wheeled Robot/twr_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/dipam/ROS-Projects/Two Wheeled Robot/twr_ws/src
+# Build directory: /home/dipam/ROS-Projects/Two Wheeled Robot/twr_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ Two Wheeled Robot/twr_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/dipam/ROS-Projects/Two Wheeled Robot/twr_ws/devel:/home/dipam/catkin_ws/devel:/opt/ros/kinetic"
+export LD_LIBRARY_PATH="/home/dipam/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-10.1/lib"
+export PWD="/home/dipam/ROS-Projects/Two Wheeled Robot/twr_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/dipam/ROS-Projects/Two Wheeled Robot/twr_ws/devel/share/common-lisp:/home/dipam/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/dipam/ROS-Projects/Two Wheeled Robot/twr_ws/src:/home/dipam/catkin_ws/src:/opt/ros/kinetic/share"
